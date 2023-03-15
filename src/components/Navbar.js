7706092059ec51272d8a188f9cf3bc0a162cba71
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-15 15:12:21 +0000

    updated navbar links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,16 +2,43 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const [click, setClick] = useState(false)
+  const handleClick = () => setClick(!click)
+  const closeMobileMenu = () => setClick(false)
+
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
           <Link to="/" className="navbar-logo">
-            TLChambers
+            {/* TLChambers <i className="fab fa-typo3" /> */}
+            TLChambers Calligraphy
           </Link>
-          <div className="menu-icon">
-            <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars" }></i>
           </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                HOME
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/services" className="nav-links" onClick={closeMobileMenu}>
+                SERVICES
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/products" className="nav-links" onClick={closeMobileMenu}>
+                PRODUCTS
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+                ABOUT
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
