27a9e933b5831c215ea22157a70fcf4e3a82ad80
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-15 17:19:16 +0000

    saving work before eod

---
+++ src/components/HeroSection.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./HeroSection.css";
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <video src="/vid/video-3.mp4" autoPlay loop muted />
+      <h1>I am simply an appreciator of beautiful writing</h1>
+      <p>
+        I am aspiring calligrapher who enjoys creating contemporary lettering
+        designs
+      </p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          ABOUT ME
+        </Button>
+        <Button
+          className="btns"
+          buttonStyle="btn--primary"
+          buttonSize="btn--large"
+          onClick={console.log("hey")}
+        >
+          WATCH TRAILER <i className="far fa-play-circle" />
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
