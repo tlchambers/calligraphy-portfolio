42ddea31d4c3def1f79596bdfd11d5104872b76d
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-17 23:42:29 +0000

    Card component created

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -7,9 +7,11 @@ function CardItem() {
     <li className="card__item">
       <Link className="cards__item__link">
         <figure className="cards__items__pic-wrap">
-        <img src="/" alt="Travel Image" className="cards__items__img">
-        </img>
+        <img src="/" alt="Travel Image" className="cards__items__img" />
         </figure>
+        <div className="cards__items__info">
+        <h5 className="cards__items__text"/>
+        </div>
       </Link>
     </li>
     </>
