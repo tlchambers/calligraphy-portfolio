009468d62e6227339d3e905e7992ecaa176a1f66
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-17 23:38:57 +0000

    Update code

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 
 function CardItem() {
   return (
-    <div>CardItem</div>
+    <>
+    <li className="card__item">
+      <Link className="cards__item__link">
+        <figure className="cards__items__pic-wrap">
+        <img src="/" alt="Travel Image" className="cards__items__img">
+        </img>
+        </figure>
+      </Link>
+    </li>
+    </>
   )
 }
 
