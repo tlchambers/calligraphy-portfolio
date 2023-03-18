a5d245acf791b5dd5edcf8df767087e6cce83a85
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-18 22:41:26 +0000

    Update code

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../../App.css';
+import CardItem from "../CardItem";
+import Cards from "../Cards";
 import HeroSection from '../HeroSection';
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <Cards />
     </>
   );
 }
