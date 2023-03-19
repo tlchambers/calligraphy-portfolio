45bbbfb7447c7b29f76b7848941a8eedd5ab88c9
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-19 09:58:17 +0000

    Added footer code

---
+++ src/components/Footer.js
@@ -0,0 +1,92 @@
+import React from "react";
+import { Button } from "./Button";
+import "./Footer.css";
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+          Join my newsletter to follow my journey!
+        </p>
+        <p className="footer-subscription-text">
+          You can unsubscibe at any time.
+        </p>
+        <div className="input-areas">
+          <form>
+            <input
+              type="email"
+              name="email"
+              placeholder="example@example.com"
+              className="footer-input"
+            />
+            <Button buttonStyle="btn--outline">Subscribe</Button>
+          </form>
+        </div>
+      </section>
+      <div class="footer-links">
+        <div className="footer-link-wrapper">
+          <div class="footer-link-items">
+            <h2>About Me</h2>
+            <Link to="/about">How it Started</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+          <div class="footer-link-items">
+            <h2>Contact Us</h2>
+            <Link to="/">Contact</Link>
+            <Link to="/">Support</Link>
+
+          </div>
+        </div>
+        <div className="footer-link-wrapper">
+          <div class="footer-link-items">
+            <h2>Videos</h2>
+            <Link to="/">Submit Video</Link>
+            <Link to="/">Ambassadors</Link>
+            <Link to="/">Agency</Link>
+            <Link to="/">Influencer</Link>
+          </div>
+          <div class="footer-link-items">
+            <h2>Social Media</h2>
+            <Link to="/">Instagram</Link>
+            <Link to="/">Facebook</Link>
+          </div>
+        </div>
+      </div>
+      <section class="social-media">
+        <div class="social-media-wrap">
+          <small class="website-rights">TLCHAMBERS © 2023</small>
+          <div class="social-icons">
+            <Link
+              class="social-icon-link facebook"
+              to="/"
+              target="_blank"
+              aria-label="Facebook"
+            >
+              <i class="fab fa-facebook-f" />
+            </Link>
+            <Link
+              class="social-icon-link instagram"
+              to="/"
+              target="_blank"
+              aria-label="Instagram"
+            >
+              <i class="fab fa-instagram" />
+            </Link>
+            <Link
+              class="social-icon-link tiktok"
+              to="/"
+              target="_blank"
+              aria-label="Tiktok"
+            >
+              <i class="fab fa-tiktok" />
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
