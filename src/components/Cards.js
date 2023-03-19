8808e355ca0753f6fbad48d601cc790857af8792
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-19 09:30:33 +0000

    Need to amend cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,41 +5,41 @@ import "./Cards.css";
 function Cards() {
   return (
     <div className="cards">
-      <h1>Check out my cards!</h1>
+      <h1>My Calligraphy</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
             <CardItem
               src="images/img-1.JPG"
-              text="Drawing on lemons"
-              label="What should I say here?"
+              text="How it all started"
+              label="Product"
+              path="/products"
+            />
+            <CardItem
+              src="images/img-2.jpg"
+              text="Travel through the Islands of Bali in a Private Cruise"
+              label="Services"
               path="/services"
             />
-            <CardItem
-              src='images/img-2.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
-              label='Luxury'
-              path='/services'
-            />
           </ul>
-          <ul className='cards__items'>
+          <ul className="cards__items">
             <CardItem
-              src='images/img-3.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-              label='Mystery'
-              path='/services'
+              src="images/img-3.jpg"
+              text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
+              label="Candles"
+              path="/candles"
             />
             <CardItem
-              src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
-              label='Adventure'
-              path='/products'
+              src="images/img-4.jpg"
+              text="Experience Football on Top of the Himilayan Mountains"
+              label="Greeting Cards"
+              path="/greeting-cards"
             />
             <CardItem
-              src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
-              label='Adrenaline'
-              path='/sign-up'
+              src="images/img-8.jpg"
+              text="Ride through the Sahara Desert on a guided camel tour"
+              label="SignUp"
+              path="/sign-up"
             />
           </ul>
         </div>
