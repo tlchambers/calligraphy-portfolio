71747e67d640e7e8df53a5cd3b98e9547b13893f
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-20 18:12:21 +0000

    image carousel

---
+++ src/components/SliderData.js
@@ -0,0 +1,18 @@
+export const SliderDate = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1557676715-93b39337b8ee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1564630322990-4a9e93d13946?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1575833947895-6c6223316549?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2128&q=80"
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1573844851308-2a3ac2622258?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=780&q=80"
+  },
+];
