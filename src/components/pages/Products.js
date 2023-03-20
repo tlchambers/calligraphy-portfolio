84626084149a9733fad0e963ecba0aabd1a19cf3
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-20 18:45:53 +0000

    Slider now working

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -7,12 +7,37 @@ const Products = ({ slides }) => {
   const [current, setCurrent] = useState(0);
   const length = slides.length;
 
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+  const prevSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current - 1);
+  };
+  console.log(current);
+
+  if (!Array.isArray(slides) || slides.legnth <= 0) {
+    return null;
+  }
+
   return (
     <section className="slider">
-      <AiOutlineArrowLeft className="left-arrow" />
-      <AiOutlineArrowRight className="right-arrow" />
+      <AiOutlineArrowLeft className="left-arrow" onClick={prevSlide} />
+      <AiOutlineArrowRight className="right-arrow" onClick={nextSlide} />
       {SliderDate.map((slide, index) => {
-        return <img src={slide.image} alt="calligraphy image" />;
+        return (
+          <div
+            className={index === current ? "slide active" : "slide"}
+            key={index}
+          >
+            {index === current && (
+              <img
+                src={slide.image}
+                alt="calligraphy image"
+                className="image"
+              />
+            )}
+          </div>
+        );
       })}
     </section>
   );
