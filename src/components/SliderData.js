19d0ef6546fa34065a3e1fea71f8f90a9a845f1d
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-20 18:46:23 +0000

    change images

--- src/components/SliderData.js
+++ src/components/SliderData.js
@@ -1,18 +1,14 @@
 export const SliderDate = [
   {
     image:
-      "https://images.unsplash.com/photo-1557676715-93b39337b8ee?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-  },
-  {
-    image:
       "https://images.unsplash.com/photo-1564630322990-4a9e93d13946?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
+    },
+  {
+    image:
+    "https://images.unsplash.com/photo-1575210489639-61c86a0154d4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1544&q=80"
   },
   {
     image:
-      "https://images.unsplash.com/photo-1575833947895-6c6223316549?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2128&q=80"
-  },
-  {
-    image:
-      "https://images.unsplash.com/photo-1573844851308-2a3ac2622258?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=780&q=80"
-  },
+    "https://images.unsplash.com/photo-1535381273077-21e00c27b1b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80"
+  }
 ];
