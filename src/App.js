69879a74fa70cffed5d0b40afc44cd86ef4bb6f0
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-21 10:15:47 +0000

    Amend signup to contact

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import "./App.css";
 import Home from "./components/pages/Home";
 import Services from "./components/pages/Services";
 import Products from "./components/pages/Products";
-import SignUp from "./components/pages/SignUp";
+import Contact from "./components/pages/Contact";
 import About from "./components/pages/About";
-import {SliderDate} from "./components/SliderData";
+import { SliderDate } from "./components/SliderData";
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
           <Route path="/services" element={<Services />} />
           <Route path="/products" element={<Products slides={SliderDate} />} />
           <Route path="/about" element={<About />} />
-          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </>
