07c60df38991075e8e586a0850a8f0a9ad59290e
Thuy Ly-Chambers <thuylychambers@hotmail.com>
2023-03-25 09:13:51 +0000

    Updated page layout

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,5 +2,18 @@ import React from 'react';
 import '../../App.css';
 
 export default function About() {
-  return <h1 className="about">About Me</h1>;
+  return (
+  <>
+    <div className="container2">
+        <div className="info-container">
+          <h1>About Me</h1>
+          <p>Receiving a lovingly calligraphed gift in the modern age of technology is something special and wonderful. It could be a personalised gift made with traditional ink and a nib, or it could be a beautifully written envelope.</p>
+          <p>Learning calligraphy has been an amazing journey for me, one that requires practice and patience. Through this journey, I have been able to create stunning works of art that showcase my unique style and creativity. </p>
+        </div>
+        <div>
+          <img className="product-image-1" src="https://images.squarespace-cdn.com/content/v1/5a3c5739aeb625ba533819a0/1614987099903-7CHLIF50E831MLTJB7EH/bespoke-calligraphy-prints-poem-commission-anniversary-gift.jpg?format=1500w" alt="calligraphy topper created in January 2023" />
+        </div>
+      </div>
+  </>
+  );
 }
--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -11,7 +11,7 @@ function Services() {
       <div className="container">
         <div className="image">
           <img
-            className="product-image-1"
+            className="product-image-1-left"
             src="https://images.squarespace-cdn.com/content/v1/5bc71a1bc2ff61688a44ba37/1585122323611-78VKAAFFLYFBNX963SLW/31.jpg?format=2500w"
             alt="image"
           />
@@ -22,25 +22,24 @@ function Services() {
       </div>
 
       <div className="container1">
-        <div className="info-container">
+        <div className="text-container">
+        <h2>Custom</h2>
           <p>Receiving a lovingly calligraphed gift in the modern age of technology is something special and wonderful. It could be a personalised gift made with traditional ink and a nib, or it could be a beautifully written envelope.</p>
           <p>Learning calligraphy has been an amazing journey for me, one that requires practice and patience. Through this journey, I have been able to create stunning works of art that showcase my unique style and creativity. </p>
         </div>
         <div>
-          <img className="product-image-1" src="https://images.squarespace-cdn.com/content/v1/5a3c5739aeb625ba533819a0/1614987099903-7CHLIF50E831MLTJB7EH/bespoke-calligraphy-prints-poem-commission-anniversary-gift.jpg?format=1500w" alt="calligraphy topper created in January 2023" />
+          <img className="product-image-100" src="https://images.squarespace-cdn.com/content/v1/5a3c5739aeb625ba533819a0/1614987099903-7CHLIF50E831MLTJB7EH/bespoke-calligraphy-prints-poem-commission-anniversary-gift.jpg?format=1500w" alt="calligraphy topper created in January 2023" />
         </div>
       </div>
 
-      <div className="container2">
-        <div className="image">
-          <img
-            className="product-image-1"
-            src="https://images.squarespace-cdn.com/content/v1/5a3c5739aeb625ba533819a0/1621346364480-BATHA2VIOKEADSKYKAGK/custom-modern-calligraphy-commission.jpg?format=1500w"
-            alt="ang"
-          />
+      <div className="container1">
+        <div>
+          <img className="product-image-100" src="https://images.squarespace-cdn.com/content/v1/5a3c5739aeb625ba533819a0/1614987099903-7CHLIF50E831MLTJB7EH/bespoke-calligraphy-prints-poem-commission-anniversary-gift.jpg?format=1500w" alt="calligraphy topper created in January 2023" />
         </div>
-        <div className="heading">
-          <h1>Bespoke calligraphy created I've built over the past 2 years</h1>
+        <div className="text-container">
+          <h2>Custom</h2>
+          <p>Receiving a lovingly calligraphed gift in the modern age of technology is something special and wonderful. It could be a personalised gift made with traditional ink and a nib, or it could be a beautifully written envelope.</p>
+          <p>Learning calligraphy has been an amazing journey for me, one that requires practice and patience. Through this journey, I have been able to create stunning works of art that showcase my unique style and creativity. </p>
         </div>
       </div>
 
